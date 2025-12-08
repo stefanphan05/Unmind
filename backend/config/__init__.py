1cc37db14f67d967d10b8e3dd4a4347726062d0b
Nguyen Tuan Hung (Stefan) Phan <phany036@mymail.unisa.edu.au>
2025-12-08 11:17:07 +1030

    Remove path in config.init

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,15 +1,6 @@
-import sys
-import os
-
 from flask import Flask
 from .settings import Config
 from .extensions import db, cors, mail
-
-
-# Ensure the backend directory is in the path
-backend_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if backend_dir not in sys.path:
-    sys.path.insert(0, backend_dir)
 
 
 def create_app():
